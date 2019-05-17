e0a27ed53ee12f3c03ce38c34ff77fd8a0278d16
Alexispe28 <Alexisprz45@hotmail.com>
2019-05-17 00:04:54 -0500

    Modificacion de Actualizar y otros cambios

---
+++ Migrations/20190517043453_Inicial.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Programacion_1.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id_Categoria = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id_Categoria);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id_Cliente = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    Correo = table.Column<string>(nullable: false),
+                    Numero_Telefonico = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id_Cliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    Id_Factura = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cantidad = table.Column<int>(nullable: false),
+                    Fecha_Realizada = table.Column<string>(nullable: true),
+                    Subtotal = table.Column<decimal>(nullable: false),
+                    Total = table.Column<decimal>(nullable: false),
+                    Id_Producto = table.Column<int>(nullable: false),
+                    Id_Cliente = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.Id_Factura);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    Id_Marca = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.Id_Marca);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producto_Proveedors",
+                columns: table => new
+                {
+                    Id_Producto = table.Column<int>(nullable: false),
+                    Id_Proveedor = table.Column<int>(nullable: false),
+                    Cantidad = table.Column<int>(nullable: false),
+                    Fecha_de_Entrega = table.Column<string>(nullable: false),
+                    Fecha_de_Salida = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto_Proveedors", x => new { x.Id_Producto, x.Id_Proveedor });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id_Producto = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Precio = table.Column<string>(nullable: false),
+                    Id_Categoria = table.Column<int>(nullable: false),
+                    Id_Marca = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id_Producto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proveedors",
+                columns: table => new
+                {
+                    Id_Proveedor = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Correo = table.Column<string>(nullable: false),
+                    Numero_Telefonico = table.Column<string>(nullable: false),
+                    Detalles = table.Column<string>(nullable: false),
+                    Direccion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedors", x => x.Id_Proveedor);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+
+            migrationBuilder.DropTable(
+                name: "Producto_Proveedors");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Proveedors");
+        }
+    }
+}
