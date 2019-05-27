0fdbdeac9d5a5be68885b5399b774cddb1021cfe
alexispe28 <alexis_perez@usmp.pe>
2019-05-27 00:50:15 -0500

    Registrar Inventario Interfaz

--- Controllers/InventarioController.cs
+++ Controllers/InventarioController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Programacion_1.Models;
+using Newtonsoft.Json;
 
 namespace Programacion_1.Models
 {
@@ -11,6 +12,11 @@ namespace Programacion_1.Models
 
         public InventarioController(ProyectoContext context) {
             _context = context;
+        }
+        public IActionResult Registrar(){
+            ViewBag.Proveedor = _context.Proveedors.ToList();
+            ViewBag.Producto = _context.Productos.ToList();
+            return View();
         }
     }
 }
